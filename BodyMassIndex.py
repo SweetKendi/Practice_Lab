11a62281199a8939f4c9bf129616bd7db05482de
james <ejpasco.22@ichat.sp.edu.sg>
2023-06-22 00:33:26 +0800

    changes for lab 3 made

--- BodyMassIndex.py
+++ BodyMassIndex.py
@@ -4,10 +4,13 @@ def calculateBMI(height, weight):
     bmi = weight/(height**2)
     if bmi<18.5:
         print("Your bmi is " + str(bmi) + "and you are under weight")
+        return -1
     elif 18.5 <=bmi<= 25.0:
         print("Your bmi is " + str(bmi) + "and you are normal weight")
+        return 0
     elif bmi > 25.0:
         print("Your bmi is " + str(bmi) + "and you are over weight")
-    print("Your bmi is " + str(bmi))
+        return 1
+
 
 calculateBMI(height=1.73, weight=60)
